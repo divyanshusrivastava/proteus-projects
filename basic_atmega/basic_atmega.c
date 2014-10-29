de31e1717d973758dad2f491c3eca48fc8e3c363
Divyanshu Srivastava <divyanshu@rewaveapp.com>
2014-10-30 00:04:36 +0530

    Basic led program AVR commit

--- basic_atmega/basic_atmega.c
+++ basic_atmega/basic_atmega.c
@@ -2,13 +2,13 @@
 #include<util/delay.h>
 
 void main() {
-	DDRA = 0x01;
+	DDRD = 0x03;
 
 	while (1) {
-		PORTA = 0x01;
-		_delay_ms(5000);
+		PORTD = 0x01;
+		_delay_ms(1000);
 
-		PORTA = 0x00;
-		_delay_ms(5000);
+		PORTD = 0x02;
+		_delay_ms(1000);
 	}
 }
